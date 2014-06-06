e85c315538aa578c9d5d97e4c9e8ade8907a33b5
Yargo Kl'ASUS <yargo+git-klasus@jerq.org>
2014-06-07 01:41:26 +0200

    added capital letters to Morse code table

--- src/morse/chargen.sh
+++ src/morse/chargen.sh
@@ -16,6 +16,32 @@ calclen() {
 }
 # third column in following table is comment and will be ignored by calclen
 cat <<EOT | calclen >$chartab
+A .-
+B -...
+C -.-.
+D -..
+E .
+F ..-.
+G --.
+H ....
+I ..
+J .---
+K -.-
+L .-..
+M --
+N -.
+O ---
+P .--.
+Q --.-
+R .-.
+S ...
+T -
+U ..-
+V ...-
+W .--
+X -..-
+Y -.--
+Z --..
 a .-
 b -...
 c -.-.
